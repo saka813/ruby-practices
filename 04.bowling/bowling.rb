c4c272c656a2df40ad6bf4e6151b7f101afd8cd5
saka813 <rie.osaka@gmail.com>
2022-09-04 18:20:08 +0900

    revise

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
+scores = ARGV[0].split(',')
 shots = []
+
+# 1投ずつshots配列に格納。ストライクは10,0とする。
 scores.each do |s|
   if s == 'X'
     shots << 10
@@ -12,6 +13,7 @@ scores.each do |s|
   end
 end
 
+# 9フレームまで、2投(1フレーム)ずつframes配列に格納
 frames = []
 shots.first(18).each_slice(2) do |s|
   frames << s
@@ -19,14 +21,15 @@ end
 frames << shots.drop(18)
 
 point = 0
+all_pins = 10
 
 frames.first(8).each_with_index do |frame, i|
-  point += if frame[0] == 10 && frames.slice(i + 1)[0] == 10 # strike & strike
-             frame.sum + 10 + frames.slice(i + 2)[0]
-           elsif frame[0] == 10 # strike
-             frame.sum + frames.slice(i + 1)[0] + frames.slice(i + 1)[1]
-           elsif frame.sum == 10 # spair
-             frame.sum + frames.slice(i + 1)[0]
+  point += if frame[0] == all_pins && frames[i + 1][0] == all_pins # strike & strike
+             frame.sum + all_pins + frames[i + 2][0]
+           elsif frame[0] == all_pins # strike
+             frame.sum + frames[i + 1][0] + frames[i + 1][1]
+           elsif frame.sum == all_pins # spair
+             frame.sum + frames[i + 1][0]
            else
              frame.sum
            end
@@ -34,19 +37,17 @@ end
 
 ninth = frames[8]
 last = frames[9]
-last_point = last.sum
 
-ninth_point = if ninth[0] == 10 && last[0] == 10 && last[2] == 10 # strike & strike & strike
-                (ninth.sum + 20)
-              elsif ninth[0] == 10 && last[0] == 10 && last[2] != 10 # stirke & strike
-                ninth.sum + 10 + last[2]
-              elsif ninth[0] == 10  # strike
+ninth_point = if ninth[0] == all_pins && last[0] == all_pins # strike & strike
+                ninth.sum + all_pins + last[2]
+              elsif ninth[0] == all_pins  # strike
                 ninth.sum + last[0] + last[1]
-              elsif ninth.sum == 10 # spair
+              elsif ninth.sum == all_pins # spair
                 ninth.sum + last[0]
               else
                 ninth.sum
               end
 
+last_point = last.sum
 point = point + ninth_point + last_point
 puts point
