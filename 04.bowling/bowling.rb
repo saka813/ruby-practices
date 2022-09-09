42da2018622f3389eb728344b622e08bb31d60ea
saka813 <rie.osaka@gmail.com>
2022-09-09 19:57:28 +0900

    revise

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# 10本のピン全てを定数ALL_PINSと定義
+ALL_PINS = 10
+
 scores = ARGV[0].split(',')
 shots = []
 
@@ -14,21 +17,17 @@ scores.each do |s|
 end
 
 # 9フレームまで、2投(1フレーム)ずつframes配列に格納
-frames = []
-shots.first(18).each_slice(2) do |s|
-  frames << s
-end
+frames = shots.first(18).each_slice(2).to_a
 frames << shots.drop(18)
 
 point = 0
-all_pins = 10
 
 frames.first(8).each_with_index do |frame, i|
-  point += if frame[0] == all_pins && frames[i + 1][0] == all_pins # strike & strike
-             frame.sum + all_pins + frames[i + 2][0]
-           elsif frame[0] == all_pins # strike
+  point += if frame[0] == ALL_PINS && frames[i + 1][0] == ALL_PINS # strike & strike
+             frame.sum + ALL_PINS + frames[i + 2][0]
+           elsif frame[0] == ALL_PINS # strike
              frame.sum + frames[i + 1][0] + frames[i + 1][1]
-           elsif frame.sum == all_pins # spair
+           elsif frame.sum == ALL_PINS # spair
              frame.sum + frames[i + 1][0]
            else
              frame.sum
@@ -38,16 +37,15 @@ end
 ninth = frames[8]
 last = frames[9]
 
-ninth_point = if ninth[0] == all_pins && last[0] == all_pins # strike & strike
-                ninth.sum + all_pins + last[2]
-              elsif ninth[0] == all_pins  # strike
+ninth_point = if ninth[0] == ALL_PINS && last[0] == ALL_PINS # strike & strike
+                ninth.sum + ALL_PINS + last[2]
+              elsif ninth[0] == ALL_PINS  # strike
                 ninth.sum + last[0] + last[1]
-              elsif ninth.sum == all_pins # spair
+              elsif ninth.sum == ALL_PINS # spair
                 ninth.sum + last[0]
               else
                 ninth.sum
               end
 
-last_point = last.sum
-point = point + ninth_point + last_point
+point = point + ninth_point + last.sum
 puts point
